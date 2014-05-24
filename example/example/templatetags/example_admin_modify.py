b2f18c3bba85bf7983a67d8c8569d92529632c1d
Matteo Nastasi <nastasi@openquake.org>
2014-05-24 10:00:22 +0200

    object_id passed to the new custom button

--- example/example/templatetags/example_admin_modify.py
+++ example/example/templatetags/example_admin_modify.py
@@ -13,6 +13,7 @@ def example_submit_row(context):
     change = context['change']
     is_popup = context['is_popup']
     save_as = context['save_as']
+    object_id = context['object_id']
     return {
         'onclick_attrib': (opts.get_ordered_objects() and change
                             and 'onclick="submitOrderForm();"' or ''),
@@ -23,5 +24,6 @@ def example_submit_row(context):
                             not is_popup and (not save_as or context['add']),
         'show_save_and_continue': not is_popup and context['has_change_permission'],
         'is_popup': is_popup,
+        'object_id': object_id,
         'show_save': True
     }
